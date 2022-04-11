6710986f0b22358d40ed2caa291ead39195ee1a9
speyck <54800864+baltermia@users.noreply.github.com>
2022-04-11 15:19:11 +0200

    Add temperature sensor

--- src/src.ino
+++ src/src.ino
@@ -1,37 +1,56 @@
 #include <Arduino.h>
 #include <RotaryEncoder.h>
+#include <DHT.h>
 
-#define PIN_IN1 2
-#define PIN_IN2 3
+#define ROTARY1 2
+#define ROTARY2 3
 
-RotaryEncoder rotary(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+RotaryEncoder rotary(ROTARY1, ROTARY2, RotaryEncoder::LatchMode::TWO03);
+
+DHT dht(DHTPIN, DHTTYPE);
 
 // Range from 0 to 100
 static int brightness = 100;
 
+static float temperature;
+
+bool checkTemperature = true;
+
 void setup()
 {
   Serial.begin(115200);
+  Serial.begin(9600);
   while (! Serial) ;
+
+  dht.begin();
 }
 
 void loop()
+{
+  RotarySensor();
+  if (checkTemperature)
+  {
+    TemperatureSensor();
+  }
+}
+
+void RotarySensor() 
 {
   static int pos = 0;
   rotary.tick();
 
   int newPos = rotary.getPosition();
-  if (pos != newPos) 
-  {
-    RotarySensor((int)rotary.getDirection());
-    pos = newPos;
-  }
-}
 
-void RotarySensor(int direction) 
-{
-  bool isAdd = direction == 1;
-  
+  if (pos == newPos) 
+  {
+    return;
+  }
+
+  bool isAdd = (int)rotary.getDirection() == 1;
+
   if (isAdd && brightness < 100)
   {
     brightness++;
@@ -41,5 +60,25 @@ void RotarySensor(int direction)
     brightness--;
   }
 
-  Serial.println(brightness);
+  Serial.println(brightness);    
+  pos = newPos;
+}
+
+void TemperatureSensor() 
+{
+  float tmp = dht.readTemperature();
+
+  if (isnan(tmp))
+  {
+    return;
+  }
+
+  temperature = tmp;
+
+  Serial.print(temperature);
+  Serial.print(F("°C"));
+
+  Serial.println();
+
+  delay(500);
 }
