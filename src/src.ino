671abce6f7a35e2981a6678dac26fba9263397d9
baltermia <54800864+baltermia@users.noreply.github.com>
2022-04-19 18:19:57 +0200

    Add WebServer (only ON/OFF functionality)

--- src/src.ino
+++ src/src.ino
@@ -1,29 +1,49 @@
 #include <Arduino.h>
 #include <RotaryEncoder.h>
 #include <DHT.h>
+#include <WiFiNINA.h>
 
+// -- Brightness / Rotary-Sensor -----------
 #define ROTARY1 2
 #define ROTARY2 3
 
-#define DHTPIN 4
-#define DHTTYPE DHT11
-
 RotaryEncoder rotary(ROTARY1, ROTARY2, RotaryEncoder::LatchMode::TWO03);
-
-DHT dht(DHTPIN, DHTTYPE);
 
 // Range from 0 to 100
 static int brightness = 100;
+// -----------------------------------------
+
+// -- Temperature / DHT-Sensor -------------
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 static float temperature;
 
 bool checkTemperature = true;
+// -----------------------------------------
+
+// -- WebServer ----------------------------
+const char ssid[] = "username";
+const char pass[] = "password";
+
+int keyIndex = 0;
+
+WiFiServer server(80);
+// -----------------------------------------
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.begin(9600);
+  
   while (! Serial) ;
+
+  EnableWiFi();
+  ConnectWiFi();
+
+  server.begin();
+  PrintWifiStatus();
 
   dht.begin();
 }
@@ -31,10 +51,13 @@ void setup()
 void loop()
 {
   RotarySensor();
+
   if (checkTemperature)
   {
     TemperatureSensor();
   }
+
+  WebServer();
 }
 
 void RotarySensor() 
@@ -59,8 +82,7 @@ void RotarySensor()
   {
     brightness--;
   }
-
-  Serial.println(brightness);    
+   
   pos = newPos;
 }
 
@@ -79,9 +101,95 @@ void TemperatureSensor()
   }
 
   temperature = tmp;
-
-  Serial.print(temperature);
-  Serial.print(F("°C"));
-
-  Serial.println();
 }
+
+void WebServer() 
+{
+  WiFiClient client = server.available();
+
+  if (!client)
+  {
+    client.stop();
+    return;
+  }
+
+  String currentLine = "";
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read();
+      // Print whole requests to serial (DEBUG only)
+      // Serial.write(c); 
+      if (c == '\n') {
+        if (currentLine.length() == 0) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+
+          client.print("Click <a href=\"/ON\">here</a> to turn leds on<br>");
+          client.print("Click <a href=\"/OFF\">here</a> to turn leds off<br>");
+
+          client.println("Curren brightness: ");
+          client.print(brightness);
+
+          client.println("Curren temperature: ");
+          client.print(temperature);
+
+          client.println();
+          break;
+        }
+        else {
+          currentLine = "";
+        }
+      }
+      else if (c != '\r') {
+        currentLine += c;
+        }
+
+      if (currentLine.endsWith("GET /ON")) {
+        Serial.println("WebServer: ON");
+      }
+      if (currentLine.endsWith("GET /OFF")) {
+        Serial.println("WebServer: OFF");
+      }
+    }
+  }  
+  
+  client.stop();
+}
+
+// -- WiFi ---------------------------------
+void PrintWifiStatus() 
+{
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void EnableWiFi() 
+{
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+}
+
+void ConnectWiFi() 
+{
+  int status = WL_IDLE_STATUS;
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 1 seconds for connection:
+    delay(1000);
+  }
+}
+// -----------------------------------------
